6c3e458bc078fb3f47584a25b4d03be2e21d60ea
Desiree Dixon <desireedixon@Desirees-MacBook-Pro.local>
2020-12-22 20:01:52 -0600

    finishign stuff up i guess

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -12,10 +12,33 @@ export class CrewComponent implements OnInit {
     {name: "Mae Jemison", firstMission: false},
     {name: "Ellen Ochoa", firstMission: true}
   ];
+  memberBeingEdited: object = null;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  add(memberName: string, isFirst: boolean) {
+    if(!this.crew.includes(memberName)){
+      this.crew.push({name: memberName, firstMission: isFirst});
+    }
+
+  }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1)
+  }
+  
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
+
+  save(name: string, member: object) {
+  member['name'] = name;
+  this.memberBeingEdited = null;
+  }
+  
 
 }
